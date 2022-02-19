3684bb117f2a054b10e35b213b3d34b2f2629362
emmanuel <emmabasko@gmail.com>
2022-02-19 16:54:38 +0100

    My first ruby program

---
+++ testing_ruby/hello_world.rb
@@ -0,0 +1,11 @@
+puts "Hello World..!"
+
+introduction = "My name is Emmanuel Bask-Aiyedun,
+I am an upcoming software developer learning ruby.
+This is my first official ruby program."
+
+puts introduction
+print "\n"
+p "Thank You"
+
+# Just testing all the output functions of ruby
